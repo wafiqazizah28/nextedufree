bc85eeeee83a1be0a32ba675f333ff110d3cf2d8
wafiqazizah28 <wafiq9745@gmail.com>
2025-03-17 09:40:15 +0700

    commit zizi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            PertanyaanSeeder::class,
-            JurusanSeeder::class,
-            SaranPekerjaanSeeder::class,
-            ArtikelSeeder::class,
-            RuleSeeder::class,
-            HasilTesSeeder::class,
-            KategoriArtikelSeeder::class,
+            
         ]);
     }
 }
